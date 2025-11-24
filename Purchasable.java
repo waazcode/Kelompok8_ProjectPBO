ccfed5bc9bb0ecf7a851bebf786a0ce6660993e6
waazcode <fawwaz24@mhs.usk.ac.id>
2025-11-25 00:46:16 +0700

    Delete Purchasable.java

--- Purchasable.java
+++
@@ -1,9 +0,0 @@
-package Kelompok8;
-
-/**
- * Interface sebagai contoh penggunaan Abstraction via Interface.
- */
-public interface Purchasable {
-    Order placeOrder(Cart cart, Store store, PaymentMethod pm, TransferBank transferBank)
-            throws InsufficientStockException;
-}
